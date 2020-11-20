3e4e894c714e3427de7dd949844f5527427ccd06
Martin Freisehner <mfreisehner@gmail.com>
2020-11-20 21:45:33 +0100

    additional fixes including endless loop for scenarios without possible fortification actions

--- src/main/java/agents/leeroy/AttackActionSupplier.java
+++ src/main/java/agents/leeroy/AttackActionSupplier.java
@@ -43,9 +43,8 @@ public class AttackActionSupplier {
         final int maxAttackTroops = risk.getBoard().getMobileTroops(srcTerritoryId);
         final int defenderTroops = risk.getBoard().getTerritoryTroops(targetTerritoryId);
 
-        // check if we would win the battle if we move all troops - but legal moves contain max 3 troops
         if (BattleSimulator.getWinProbability(maxAttackTroops, defenderTroops) >= RISK_THRESHOLD) {
-            return Set.of(RiskAction.attack(srcTerritoryId, targetTerritoryId, Math.min(maxAttackTroops,3)));
+            return Set.of(RiskAction.attack(srcTerritoryId, targetTerritoryId, maxAttackTroops));
         }
         return Set.of();
     }
